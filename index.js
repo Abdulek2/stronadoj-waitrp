f39637546f3f7dee39427fe80c6bfa642f7087b9
Kosmo <143964847+Abdulek2@users.noreply.github.com>
2026-08-22 21:03:04 +0200

    Update index.js
    prosty blad jestem cwelem

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT  3000;
+const PORT = process.env.PORT || 3000;
 
  Zmienne środowiskowe z ustawień hostingu (Render.com)
 const CLIENT_ID = process.env.CLIENT_ID;
